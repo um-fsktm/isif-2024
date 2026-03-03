839b7134cafe075898470169ca906f6f3bc445c9
MENG HUI CHOI <49356439+skywood123@users.noreply.github.com>
2026-03-03 21:53:26 +0800

    Add files via upload

---
+++ multi-user-jupyterhub(direct expose)/p4mn-jupyter/dhcpsnooping/dhcp-server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import os
+
+from scapy.all import sniff, sendp, get_if_list, get_if_hwaddr, get_if_raw_hwaddr
+from scapy.all import Ether, IP, UDP, DHCP, BOOTP
+
+def send_response(pkt):
+	client_hw_addr = pkt[Ether].src
+	client_ip_addr = pkt[IP].src
+	print ("request detected from client with MAC: %s and IP: %s" % (client_hw_addr, client_ip_addr))
+
+	#ifaces = filter(lambda i: 'eth' in i, os.listdir('/sys/class/net/'))
+	#iface = ifaces[0]
+	iface = "eth0"
+
+	print ("sending response packet from interface %s" % iface)
+	rpkt = Ether(src = get_if_hwaddr(iface), dst = client_hw_addr)
+	rpkt = rpkt / IP(dst = client_ip_addr) / UDP(dport = 68, sport = 67) / BOOTP(op = 2, chaddr = get_if_hwaddr(iface)) / DHCP(options = [('message-type', 'ack'), ('end')])
+	sendp(rpkt, iface = iface, verbose = False)
+	rpkt.show2()
+	exit(1)
+
+def handle_pkt(pkt):
+	if UDP in pkt and pkt[UDP].dport == 67:
+		print ("got a DHCP packet from known client")
+		pkt.show2()
+		send_response(pkt)
+
+def main():
+	#ifaces = filter(lambda i: 'eth' in i, os.listdir('/sys/class/net/'))
+	#iface = ifaces[0]
+	iface = "eth0"
+	print ("sniffing on %s" % iface)
+	sys.stdout.flush()
+	sniff(iface = iface,
+		prn = lambda x: handle_pkt(x))
+	# sniff function passes the packet object as the one arg into prn: func
+
+if __name__ == '__main__':
+	main()
